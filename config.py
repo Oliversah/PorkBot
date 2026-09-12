ef6160c11bacdbfe0b40caed2c8a7741b3317410
Oliversah <brayanhenriq04@gmail.com>
2026-09-12 18:18:51 -0300

    Adiciona cargos permitidos e atualiza configuracoes

--- config.py
+++ config.py
@@ -2,7 +2,14 @@
 CANAL_PALPITES_ID = 1539698402617589760  # Substitua pelo ID do canal
 CANAL_REGISTROS_ID = 1539698492958572635  # ID do canal privado onde chegam os logs de palpites
 
+# IDs dos cargos que podem gerenciar o bolão (além de administradores)
+CARGOS_PERMITIDOS_IDS = [
+    1255222988534710332,  # Dev
+    963192709848391700,   # Adm
+]
+
 palpites_abertos = True
+resultado_divulgado = False  # Flag de controle para evitar o bug do bolão sem palpites
 mensagem_bolao_id = None
 
 embed_builder = {
